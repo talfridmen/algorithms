a54e8137f85d80bbeec89c65f09667e360e00ad9
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-17 11:37:13 +0300

    added bfs tests

--- Expert/bfs/bfs.py
+++ Expert/bfs/bfs.py
@@ -35,26 +35,50 @@ class Node:
     def __str__(self) -> str:
         return self._to_string(last=True)
 
-if __name__ == "__main__":
+def test():
+    failed = []
+    passed = []
+
+    # 1
     t0 = Node(1, [Node(2), Node(4, [Node(3)])])
-    print(search(t0, 3))  # True
+    (passed if search(t0, 3) == True else failed).append(1)
 
+    # 2
     t1 = Node(1, [Node(3, [Node(4)]), Node(2)])
-    print(search(t1, 3))  # True
+    (passed if search(t1, 3) == True else failed).append(2)
 
+    # 3
     t2 = Node(3, [Node(1, [Node(4)]), Node(2)])
-    print(search(t2, 3))  # True
-
+    (passed if search(t2, 3) == True else failed).append(3)
+    
+    # 4
     t3 = Node(1, [Node(2), Node(3, [Node(4)])])
-    print(search(t3, 3))  # True
+    (passed if search(t3, 3) == True else failed).append(4)
 
+    # 5
     t4 = Node(1, [Node(2), Node(5, [Node(4)])])
-    print(search(t4, 3))  # False
+    (passed if search(t4, 3) == False else failed).append(5)
 
+    # 6
     t5 = Node(1, [Node(2, [Node(3), Node(20)]), Node(5, [Node(6, [Node(21, [Node(8), Node(9), Node(10), Node(11)])])])])
-    print(search(t5, 2))  # True
-    print(search(t5, 4))  # False
-    print(search(t5, 6))  # True
-    print(search(t5, 7))  # False
-    print(search(t5, 10)) # True
-    print(search(t5, 11)) # True
+    (passed if search(t5, 2) == True else failed).append(6)
+    # 7
+    (passed if search(t5, 4) == False else failed).append(7)
+    # 8
+    (passed if search(t5, 6) == True else failed).append(8)
+    # 9
+    (passed if search(t5, 7) == False else failed).append(9)
+    # 10
+    (passed if search(t5, 10) == True else failed).append(10)
+    # 11
+    (passed if search(t5, 11) == True else failed).append(11)
+
+    if failed:
+        print(f"Failed tests: {failed}")
+    else:
+        print("PASSED!")
+
+if __name__ == "__main__":
+    print(Node(1, [Node(2), Node(4, [Node(3)])]))
+    test()
+    
