1f70564fa3cd9e6525a4ec54994195d0c6ac9267
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:43:35 +0300

    implemented bubble_sort patbas

--- Expert/bubble_sort/bubble_sort.py
+++ Expert/bubble_sort/bubble_sort.py
@@ -1,2 +1,11 @@
 def bubble_sort(lst):
-    pass # implement here!
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+                changed = True
+    return lst
+
+
