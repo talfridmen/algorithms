08fb7646ad09489a9b42debefcbaee4f45efa338
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:38:17 +0300

    implemented two_pointer patbas

--- Advanced/two_pointer/two_pointer.py
+++ Advanced/two_pointer/two_pointer.py
@@ -1,2 +1,8 @@
 def two_pointer(lst, num):
-    pass # Implement Here!
+    p1, p2 = 0, len(lst) - 1
+    while p2 > p1 and lst[p1] + lst[p2] != num:
+        if lst[p1] + lst[p2] < num:
+            p1 += 1
+        else:
+            p2 -= 1
+    return lst[p1] + lst[p2] == num
