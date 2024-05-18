5b5a92b59ba25a6f61815f20a37e2aeae1b151ea
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:40:37 +0300

    implemented merge_sort patbas

--- Expert/merge_sort/merge_sort.py
+++ Expert/merge_sort/merge_sort.py
@@ -1,2 +1,21 @@
 def merge_sort(lst):
-    pass # implement here!
+    if len(lst) == 1:
+        return lst
+    else:
+        l_bottom, l_top = merge_sort(lst[:len(lst)//2]), merge_sort(lst[len(lst)//2:])
+        new_l = []
+        i, j = 0, 0
+        while i < len(l_bottom) and j < len(l_top):
+            if l_bottom[i] <= l_top[j]:
+                new_l.append(l_bottom[i])
+                i += 1
+            else:
+                new_l.append(l_top[j])
+                j += 1
+        if i == len(l_bottom):
+            new_l.extend(l_top[j:])
+        elif j == len(l_top):
+            new_l.extend(l_bottom[i:])
+        else:
+            raise ValueError("1")
+        return new_l
