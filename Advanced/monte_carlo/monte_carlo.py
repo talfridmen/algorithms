103697f3796ce9ff8a7274af7785226189759b28
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:02:49 +0300

    Implemented PATBAS

--- Advanced/monte_carlo/monte_carlo.py
+++ Advanced/monte_carlo/monte_carlo.py
@@ -1,4 +1,9 @@
 import random  # might be useful ;)
 
 def monte_carlo(reps):
-    pass # Implement Here!
+    in_circle = 0
+    for i in range(reps):
+        x, y = random.random(), random.random()
+        if (x ** 2 + y ** 2) ** 0.5 <= 1:
+            in_circle += 1
+    return 4 * in_circle / reps
