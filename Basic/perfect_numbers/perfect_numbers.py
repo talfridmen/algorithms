d2205b08b543e8d7c1afafa632b67bb7c6b12753
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:11:31 +0300

    implemented perfect numbers patbas

--- Basic/perfect_numbers/perfect_numbers.py
+++ Basic/perfect_numbers/perfect_numbers.py
@@ -1,2 +1,8 @@
 def perfect_numbers(num):
-    pass # Implement Here!
+    summ = 0
+    for i in range(1, int(num ** 0.5) + 1):
+        if num % i == 0:
+            summ += i
+            if num // i > i:
+                summ += num // i
+    return summ == 2 * num
