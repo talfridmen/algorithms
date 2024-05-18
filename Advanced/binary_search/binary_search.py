942d01ff1cd809a12f261d5e02da30a8235b42d8
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:27:41 +0300

    implemented binary search patbas

--- Advanced/binary_search/binary_search.py
+++ Advanced/binary_search/binary_search.py
@@ -1,2 +1,9 @@
 def binary_search(lst, num):
-    pass # Implement Here!
+    if len(lst) == 1:
+        return 0 if lst[0] == num else None
+    if lst[len(lst) // 2] <= num:
+        val = binary_search(lst[len(lst) // 2:], num)
+        if val is not None:
+            return val + len(lst) // 2
+        return None
+    return binary_search(lst[:len(lst) // 2], num)
