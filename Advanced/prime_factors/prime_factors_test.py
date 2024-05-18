ea126ce53c4b7d818c892a15deac3f31117456b8
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:34:06 +0300

    test fix

--- Advanced/prime_factors/prime_factors_test.py
+++ Advanced/prime_factors/prime_factors_test.py
@@ -5,16 +5,16 @@ def test():
     passed = []
 
     # 1
-    (passed if set(prime_factors(5)) == {5} else failed).append(1)
+    (passed if sorted(prime_factors(5)) == [5] else failed).append(1)
     
     # 2
-    (passed if set(prime_factors(10)) == {2, 5} else failed).append(2)
+    (passed if sorted(prime_factors(10)) == [2, 5] else failed).append(2)
     
     # 3
-    (passed if set(prime_factors(100)) == {2, 2, 5, 5} else failed).append(3)
+    (passed if sorted(prime_factors(100)) == [2, 2, 5, 5] else failed).append(3)
     
     # 4
-    (passed if set(prime_factors(38220)) == {2, 2, 3, 5, 7, 7, 13} else failed).append(4)
+    (passed if sorted(prime_factors(38220)) == [2, 2, 3, 5, 7, 7, 13] else failed).append(4)
 
     if failed:
         print(f"Failed tests: {failed}")
