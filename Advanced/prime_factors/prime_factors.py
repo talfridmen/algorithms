4e55bca7b63e7b4d38bb9009f9393686e1658784
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-05-19 00:34:51 +0300

    implemented prime-factors patbas

--- Advanced/prime_factors/prime_factors.py
+++ Advanced/prime_factors/prime_factors.py
@@ -1,2 +1,10 @@
 def prime_factors(num):
-    pass # Implement Here!
+    factors = []
+    factor = 2
+    while num > 1:
+        if num % factor == 0:
+            factors.append(factor)
+            num //= factor
+        else:
+            factor += 1
+    return factors
