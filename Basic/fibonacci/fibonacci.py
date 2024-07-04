08e3e37668ad829e946f6b7dea3074d50e784e0c
eliyahu with access <eliyahu@dev-1001.pele.local>
2024-07-04 16:58:17 +0300

    Solution

--- Basic/fibonacci/fibonacci.py
+++ Basic/fibonacci/fibonacci.py
@@ -1,2 +1,15 @@
 def fibonacci(idx):
-    pass # Implement Here!
+    x = 1
+    y = 0
+    for i in range(idx):
+        y,x = x,y
+        x = x+y
+    return y
+
+
+def user_input ():
+    iteration_number = int(input("Please enter the number of wanted iterations:"))
+    return iteration_number
+
+if __name__ == "__main__":
+    print(fibonacci(user_input()))
