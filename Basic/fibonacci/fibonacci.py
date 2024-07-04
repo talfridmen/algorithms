f9196f734cb11b199c5a8ce2235940792d832973
U-EMEA\talye <talye@YEDWIBWLES.emea.hpqcorp.net>
2024-07-04 16:59:42 +0300

    implemented fibonacci

--- Basic/fibonacci/fibonacci.py
+++ Basic/fibonacci/fibonacci.py
@@ -1,2 +1,17 @@
 def fibonacci(idx):
-    pass # Implement Here!
+#jtal    pass # Implement Here!
+  x=0
+  y=1
+
+  if idx==0:
+    return x
+  elif idx==1:
+    return y
+# else
+  while idx > 1 :
+    t=x
+    x=y
+    y+=t
+    idx-=1
+
+  return y 
