901726f0e84550edf93314a5a95ef10cc4e28727
U-EMEA\talye <talye@YEDWIBWLES.emea.hpqcorp.net>
2024-07-07 17:59:19 +0300

    implemented prime_factors

--- Advanced/prime_factors/prime_factors.py
+++ Advanced/prime_factors/prime_factors.py
@@ -1,2 +1,14 @@
 def prime_factors(num):
-    pass # Implement Here!
+#    pass # Implement Here!
+    factors=[]
+    pf=2
+
+    while num > 1:
+       if num%pf == 0:
+         factors.append(pf)
+         num = num//pf
+       else:
+         pf+=1
+
+    return factors
+
