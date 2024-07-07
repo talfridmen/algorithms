6e6443e925bd07ce9ad73d2a6d3174b2b8f1eb1f
U-EMEA\talye <talye@YEDWIBWLES.emea.hpqcorp.net>
2024-07-07 16:31:36 +0300

    implemented is_prime

--- Basic/is_prime/is_prime.py
+++ Basic/is_prime/is_prime.py
@@ -1,2 +1,13 @@
 def is_prime(num):
-    pass # Implement Here!
+#jtal    pass # Implement Here!
+  if num==1:
+    return False;
+#else
+  idx=2
+  while(idx*idx <= num):   # I didn't use the sqrt math function as we still didn't talk about it.
+    if num%idx == 0:
+      return False
+    idx+=1
+#else
+  return True
+  
