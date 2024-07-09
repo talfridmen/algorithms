b2a812e4214a10f795cc16f1f8c9e411c35caee2
U-EMEA\talye <talye@YEDWIBWLES.emea.hpqcorp.net>
2024-07-09 15:06:52 +0300

    implemented binary_search

--- Advanced/binary_search/binary_search.py
+++ Advanced/binary_search/binary_search.py
@@ -1,2 +1,12 @@
 def binary_search(lst, num):
-    pass # Implement Here!
+#jtal    pass # Implement Here!
+   startIdx=0
+   endIdx=len(lst)-1
+   
+   while(startIdx<=endIdx):
+      midIdx=(startIdx+endIdx)//2
+      if lst[midIdx] == num: return midIdx
+      elif lst[midIdx] < num: startIdx=midIdx+1
+      elif lst[midIdx] > num: endIdx=midIdx-1
+
+   return None
