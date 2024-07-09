1db48e3e07bc20db0bc252cf931d530bcbdf2c4f
U-EMEA\talye <talye@YEDWIBWLES.emea.hpqcorp.net>
2024-07-09 17:52:16 +0300

    implemented two pointer and fixed two_pointer_test

--- Advanced/two_pointer/two_pointer.py
+++ Advanced/two_pointer/two_pointer.py
@@ -1,2 +1,12 @@
 def two_pointer(lst, num):
-    pass # Implement Here!
+#jtal    pass # Implement Here!
+   ilow=0
+   ihigh=len(lst)-1
+
+   while(ilow<ihigh):
+      sum=lst[ilow]+lst[ihigh]
+      if sum==num: return True
+      elif sum > num: ihigh-=1 
+      else: ilow+=1
+
+   return False
