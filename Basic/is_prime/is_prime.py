d83f615676d449f8b5f27c53223a36f7d3eb7a09
Daniel Letai <dani@letai.org.il>
2024-07-30 15:11:42 +0300

    is_prime

--- Basic/is_prime/is_prime.py
+++ Basic/is_prime/is_prime.py
@@ -1,2 +1,10 @@
 def is_prime(num):
-    pass # Implement Here!
+    from math import isqrt
+    if num == 1:
+        return False
+
+    for i in range(2,isqrt(num)+1):
+        if num%i == 0:
+            return False
+
+    return True
