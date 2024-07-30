dbfbd4665e5481e2d8e838acad64f95dcedc7646
daniel zerihon <daniel92023@gmail.com>
2024-07-30 17:07:45 +0300

    ip prime function
    Signed-off-by: daniel zerihon <daniel92023@gmail.com>

--- Basic/is_prime/is_prime.py
+++ Basic/is_prime/is_prime.py
@@ -1,2 +1,8 @@
 def is_prime(num):
-    pass # Implement Here!
+    if(num < 2): return False
+    
+    for i in range(2, int(num ** 0.5)+1):
+        if(num % i == 0):
+            return False
+        
+    return True
