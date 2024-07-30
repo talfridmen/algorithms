97636dacff3c381ff36ca026a3844ff182ccc37c
root <root@kubernetes>
2024-07-30 14:54:06 +0300

    fibonacc
    Signed-off-by: root <root@kubernetes>

--- Basic/fibonacci/fibonacci.py
+++ Basic/fibonacci/fibonacci.py
@@ -1,2 +1,24 @@
 def fibonacci(idx):
-    pass # Implement Here!
+    if(idx <= 2):
+        if(idx < 0): 
+            return -1
+        if(idx == 0): 
+            return 0
+        if(idx == 1): 
+            return 1
+        if(idx == 2): 
+            return 1
+
+    i = 2 
+    firstDigit = 1 
+    secondDigit = 2
+    tempDigit = 0
+    while(i < idx):
+        tempDigit = secondDigit
+        secondDigit += firstDigit
+        firstDigit = tempDigit
+        i = i + 1 
+    print(f"fibonacci[{idx}] = {firstDigit}")
+    return firstDigit
+
+fibonacci(10)
