31600425ead84ee95d07db6d8a831d18228fd1af
root <root@kubernetes>
2024-07-30 14:54:06 +0300

    fibonacc
    Signed-off-by: root <root@kubernetes>

--- Basic/fibonacci/fibonacci.py
+++ Basic/fibonacci/fibonacci.py
@@ -1,2 +1,13 @@
 def fibonacci(idx):
-    pass # Implement Here!
+    if(idx <= 1):
+        return idx
+
+    tempNumber = 0
+    firstNumber = 1 
+    secondNumber = 2
+
+    for i in range(2, idx):
+        tempNumber = secondNumber
+        secondNumber += firstNumber
+        firstNumber = tempNumber
+    return firstNumber
