f477a958b55a1af0b19a291a61a0d52d80079906
eliyahu with access <eliyahu@dev-1001.pele.local>
2024-07-30 12:14:50 +0300

    I created a function that checks for prime numbers

--- Basic/is_prime/is_prime.py
+++ Basic/is_prime/is_prime.py
@@ -1,2 +1,24 @@
 def is_prime(num):
     pass # Implement Here!
+
+# I'm creating 2 different variables, X recieves num as an integer and Y is set with 2 to divide it in the while loop
+    X=int(num)
+    Y=2
+
+    for X=2
+        return True
+
+# In this while loop I will check for the reminder of the dividing action, I'll use a for loop that will check for the reminder and if it is '0' it will return false, otherwise it will return true
+    
+    while (Y<X):
+        if X%Y==0:
+            return False
+        Y+=1
+
+    return True
+
+
+    
+        
+
+
