c27c775adb4432a71760f8d5d39e2645b3b20192
talfridmen <talfridmen@walla.com>
2024-07-30 16:22:36 +0300

    Revert "fibonacci"

--- Basic/fibonacci/fibonacci.py
+++ Basic/fibonacci/fibonacci.py
@@ -1,13 +1,2 @@
 def fibonacci(idx):
-    if(idx <= 1):
-        return idx
-
-    tempNumber = 0
-    firstNumber = 1 
-    secondNumber = 2
-
-    for i in range(2, idx):
-        tempNumber = secondNumber
-        secondNumber += firstNumber
-        firstNumber = tempNumber
-    return firstNumber
+    pass # Implement Here!
