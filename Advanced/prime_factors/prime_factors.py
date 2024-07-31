0b958ee8146709432591c61bad3f95c982825265
Daniel Letai <dani@letai.org.il>
2024-07-31 12:41:08 +0300

    prime_factors

--- Advanced/prime_factors/prime_factors.py
+++ Advanced/prime_factors/prime_factors.py
@@ -1,2 +1,16 @@
 def prime_factors(num):
-    pass # Implement Here!
+    factors = []
+    if -3<=num<=3 :
+        factors.append(num)
+        return factors
+
+    i = 2
+
+    while num != 1 and num != -1:
+        if num%i == 0:
+            factors.append(i)
+            num = num//i
+        else:
+            i+=1
+
+    return factors
