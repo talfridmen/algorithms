c6d8d6adc4150a46660d3688e6775434ab8a621a
Daniel Letai <dani@letai.org.il>
2024-07-31 14:33:40 +0300

    binary_search

--- Advanced/binary_search/binary_search.py
+++ Advanced/binary_search/binary_search.py
@@ -1,2 +1,19 @@
 def binary_search(lst, num):
-    pass # Implement Here!
+    from statistics import mean
+    first_i, last_i, i = 0, len(lst)-1, len(lst)//2
+    
+    while first_i < last_i:
+        i = int(mean([last_i,first_i]))
+        if lst[i] == num:
+            return i
+        elif lst[i] > num:
+            if last_i == i:
+                return None
+            last_i = i
+        else:
+            if first_i == i:
+                return None
+            first_i = i
+
+    return None
+
