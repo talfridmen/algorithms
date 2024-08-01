ebc099b4d8b471b364bee7678dca3ee27b5574cd
Daniel Letai <dani@letai.org.il>
2024-08-02 00:05:26 +0300

    monte_carlo

--- Advanced/monte_carlo/monte_carlo.py
+++ Advanced/monte_carlo/monte_carlo.py
@@ -1,4 +1,9 @@
 import random  # might be useful ;)
 
 def monte_carlo(reps):
-    pass # Implement Here!
+    counter = 0
+    for i in range(1,reps):
+        x,y = random.random(), random.random()
+        if x**2 + y**2 <= 1:
+            counter += 1
+    return 4*counter/reps
