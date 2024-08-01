8db6b056b94789391cfe76b210d20f3e4769e1ed
Daniel Letai <dani@letai.org.il>
2024-08-02 01:32:45 +0300

    monte_carlo

--- Advanced/monte_carlo/monte_carlo.py
+++ Advanced/monte_carlo/monte_carlo.py
@@ -1,9 +1,10 @@
 import random  # might be useful ;)
 
 def monte_carlo(reps):
-    counter = 0
-    for i in range(1,reps):
-        x,y = random.random(), random.random()
-        if x**2 + y**2 <= 1:
-            counter += 1
-    return 4*counter/reps
+    import numpy as np # going to do everything in one line to make your eyes hurt
+    return 4*np.count_nonzero(np.sum(np.square(np.random.rand(reps,2)), axis=1) <= 1.0)/reps
+    # First we create an array with reps rows and 2 columns of random numbers between 0,1.0 : np.random.rand(reps,2)
+    # next we square all elements : np.square()
+    # next we sum all columns in each row : no.sum(array, axis=1) -> our 2D array is now 1D with each element is sum(square)
+    # next we count all elemnts that return True for the condition <= 1.0, using the fact True == 1 and False == 0 : np.count_nonzero(array <= 1.0)
+
